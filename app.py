285332967a6ee93ffddef297974a021d55d6fc5b
Arturo956 <60413703+Arturo956@users.noreply.github.com>
2020-06-22 21:02:04 -0500

    Update app.py
    This is my page word for word instead of editing single lines

--- app.py
+++ app.py
@@ -10,28 +10,13 @@ app = Flask(__name__)
 def hello_world():
     return current_app.send_static_file('layout.html')
 
-@app.route('/search/')
 @app.route('/search/<searchterm>')
-'''def search(searchterm = None):
-    if searchterm == None:
-        tmp = '[]'
-        return jsonify(tmp)
-    if(searchterm[0] == '"'):
-        # Phrasal Search
-        results = test2.phrasal_search(searchterm)
-    else:
-        results = test2.cosine(searchterm)'''
-
-#added code here
 def search(searchterm):
     if(searchterm[0] == '"'):
-        # Phrasal Search
-        # results = test2.phrasal_search(searchterm)
         results, words = test2.phrasal_search(searchterm)
 
     else:
         results, words = test2.cosine(searchterm)
-
     message = {
         'status': 200,
         'message': 'OK',
@@ -39,12 +24,14 @@ def search(searchterm):
     }
     col = []
 
-    for i,j in results.items():
-        titDesc=test2.titleDesc(i,words)
+    # print(words)
+
+    for i, j in results.items():
+        titDesc = test2.titleDesc(i, words)
         test = {}
         test["page"] = i
-        test["title"]= titDesc[0]
-        test["desc"]= titDesc[1]
+        test["title"] = titDesc[0]
+        test["desc"] = titDesc[1]
         test["value"] = j
         col.append(test)
 
@@ -55,7 +42,6 @@ def search(searchterm):
 
 @app.route('/<path:path>')
 def static_proxy(path):
-  # send_static_file will guess the correct MIME type
   return app.send_static_file(path)
 
 if __name__ == '__main__':
